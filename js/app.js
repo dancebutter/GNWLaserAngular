879d46a477e8cbd26666d22e282d5d20ff5ec5e5
dancebutter <ken.zhangtq@gmail.com>
2015-01-02 23:59:48 -0800

    init views

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-angular.module("mainModule", []);
+angular.module("mainModule", ['ui.router'])
+	.config(function($stateProvider, $urlRouterProvider) {
+		$urlRouterProvider.otherwise('/home');
+
+		$stateProvider
+			.state('home', {
+				url: '/home',
+				templateUrl: 'views/home.html'
+			})
+			.state('news', {
+				url: '/news',
+				templateUrl: 'views/news.html'
+			})
+			.state('laserService', {
+				url: '/laserservice',
+				templateUrl: 'views/laserservice.html'
+			});
+	});
